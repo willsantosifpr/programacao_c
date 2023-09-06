b46ce5104e19e8c60d4f394b77700a1338a1b64b
William C S Santos <willappleid@yahoo.com>
2023-09-06 12:18:33 -0300

    Programação C

---
+++ Aula 182 Vitória e derrota no jogo campo minado - Campo Minado Parte 5/main.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//William Cardoso da Silva Santos - Sep/6/2023
+
+/*  CELULA
+    ehBomba 0 ou 1
+    estaAberta 0 ou 1
+    vizinhos 0 a 4      */
+typedef struct
+{
+    int ehBomba;
+    int estaAberta;
+    int vizinhos;
+
+}Celula;
+
+// Variaveis globais
+Celula jogo[10][10];
+int l, c, tam = 10;
+
+// Procedimento para inicializar a matriz jogo
+void inicializarJogo()
+{
+    for(l= 0; l<tam; l++)
+    {
+        for(c=0; c<tam; c++)
+        {
+            jogo[l][c].ehBomba = 0;
+            jogo[l][c].estaAberta = 0;
+            jogo[l][c].vizinhos = 0;
+        }
+    }
+}
+
+// Procedimento para sortear N bombas
+void sortearBombas(int n)
+{
+    int i;
+    srand(time(NULL));
+    for(i=1; i<=n; i++)
+    {
+        l = rand() % tam;
+        c = rand() % tam;
+        if(jogo[l][c].ehBomba == 0)
+        {
+            jogo[l][c].ehBomba = 1;
+        }
+        else
+        {
+            i--;
+        }
+    }
+}
+
+/*  Funcao que diz se um par de coordenadas eh valido ou nao
+    1 == eh valido
+    0 == nao eh valido*/
+int coordenadaEhValida(int l, int c)
+{
+    if(l >= 0 && l < tam && c >= 0 && c < tam)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0 ;
+    }
+}
+
+/*  Funcao que retorna a quantidade de bombas na vizinhanca de l c
+    N == quantidade de bombas */
+int quantBombasVizinhas(int l, int c)
+{
+    /*  l - 1 e c
+        l + 1 e c
+        l e c + 1
+        l e c - 1
+    */
+
+    int quantidade = 0;
+
+    if(coordenadaEhValida(l-1, c) && jogo[l-1][c].ehBomba)
+    {
+        quantidade++;
+    }
+    if(coordenadaEhValida(l+1, c) && jogo[l+1][c].ehBomba)
+    {
+        quantidade++;
+    }
+    if(coordenadaEhValida(l, c+1) && jogo[l][c+1].ehBomba)
+    {
+        quantidade++;
+    }
+    if(coordenadaEhValida(l, c-1) && jogo[l][c-1].ehBomba)
+    {
+        quantidade++;
+    }
+
+    return quantidade;
+}
+
+// Procedimento para contar as bombas vizinhas
+void contarBombas()
+{
+    for(l=0; l<tam; l++)
+    {
+        for(c=0; c<tam; c++)
+        {
+            jogo[l][c].vizinhos = quantBombasVizinhas(l, c);
+        }
+    }
+}
+
+// Procedimento para imprimir o jogo
+void imprimirJogo()
+{
+    printf("\n\t    ");
+    for(l=0; l<tam; l++)
+    {
+        printf(" %d  ", l); // indices das colunas
+    }
+    printf("\n\t   -----------------------------------------\n");
+    for(l=0; l<tam; l++)
+    {
+        printf("\t%d  |", l); // indices das linhas
+        for(c=0; c<tam; c++)
+        {
+            if(jogo[l][c].estaAberta)
+            {
+                if(jogo[l][c].ehBomba)
+                {
+                    printf(" * ");
+                }
+                else
+                {
+                    printf(" %d ", jogo[l][c].vizinhos);
+                }
+            }
+            else
+            {
+                printf("   ");
+            }
+            printf("|");
+        }
+        printf("\n\t   -----------------------------------------\n");
+    }
+
+}
+
+// Procedimento para abrir a coordenada digitada pelo usuario
+void abrirCelula(int l, int c)
+{
+    if(coordenadaEhValida(l, c) == 1 && jogo[l][c].estaAberta == 0)
+    {
+        jogo[l][c].estaAberta = 1;
+        if(jogo[l][c].vizinhos == 0)
+        {
+            /*
+                l-1 e c vizinho acima
+                l+1 e c vizinho abaixo
+                l e c-1 vizinho a esquerda
+                l e c+1 vizinho a direita
+            */
+            abrirCelula(l-1, c);
+            abrirCelula(l+1, c);
+            abrirCelula(l, c-1);
+            abrirCelula(l, c+1);
+        }
+    }
+}
+
+/*  Funcao para verificar vitoria
+    n == ganhou
+    0 == nao ganhou ainda       */
+int ganhou()
+{
+    int quant = 0;
+
+    for(l=0; l<tam; l++)
+    {
+        for(c=0; c<tam; c++)
+        {
+            if(jogo[l][c].estaAberta == 0 && jogo[l][c].ehBomba == 0)
+            {
+                quant++;
+            }
+        }
+    }
+    return quant;
+}
+
+// Procedimento jogar que faz a leitura das coordenadas
+void jogar()
+{
+    int linha, coluna;
+
+    do
+    {
+        imprimirJogo();
+        do
+        {
+            printf("\nDigite as coordenadas de linha e coluna: ");
+            scanf("%d%d", &linha, &coluna);
+
+            if(coordenadaEhValida(linha, coluna) == 0 || jogo[linha][coluna].estaAberta == 1)
+            {
+                printf("\nCoordenadas invalida!\n");
+            }
+        }
+        while(coordenadaEhValida(linha, coluna) == 0 || jogo[linha][coluna].estaAberta == 1);
+
+        abrirCelula(linha, coluna);
+    }while(ganhou() != 0 && jogo[linha][coluna].ehBomba == 0);
+
+    imprimirJogo();
+
+    if(jogo[linha][coluna].ehBomba == 1)
+    {
+        printf("\nQue pena! Voce perdeu!!!\n");
+    }
+    else
+    {
+        printf("\n\tPARABENS! VOCE GANHOU!!!\n");
+    }
+
+}
+
+int main()
+{
+    int opcao;
+
+    do
+    {
+        inicializarJogo();
+
+        sortearBombas(10);
+
+        contarBombas();
+
+        printf("\n\t\t\tCAMPO MINADO\n");
+
+        jogar();
+
+        printf("\nDigite 1 para jogar novamente: ");
+        scanf("%d", &opcao);
+
+    }while(opcao == 1);
+
+    return 0;
+}
