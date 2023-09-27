05acec082c6e7050a05ec8239b78f0cbc50ba600
William C S Santos <willappleid@yahoo.com>
2023-09-27 17:16:55 -0300

    Programação C

---
+++ Aula 203 Ler nome e nota de três estudantes e imprimir uma tabela/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//William Cardoso da Silva Santos - Sep/23/2023
+
+int main()
+{
+    char nomes[3][35];
+    float notas[3];
+
+    for(int i=0; i<3; i++)
+    {
+        printf("Digite nome e nota do estudante: ");
+        scanf("%s%f", &nomes[i], &notas[i]);
+    }
+
+    printf("\n\tNOME\tNOTA\n");
+    for(int i=0; i<3; i++)
+    {
+        printf("\t%s\t%.2f\n", nomes[i], notas[i]);
+    }
+
+    return 0;
+}
