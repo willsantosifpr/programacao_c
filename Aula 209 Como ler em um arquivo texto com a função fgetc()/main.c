36a9f1838e3d5fc58a21ff53b72b9e6c5ae71609
William C S Santos <willappleid@yahoo.com>
2023-10-07 20:56:13 -0300

    Programação C

---
+++ Aula 209 Como ler em um arquivo texto com a função fgetc()/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//William Cardoso da Silva Santos - Oct/7/2023
+
+int main()
+{
+    FILE *file;
+    char letra;
+
+    file = fopen("conto.txt", "w+");
+
+    if(file)
+    {
+        printf("Digite um texto e pressione enter ao finalizar:\n");
+        scanf("%c", &letra);
+        while(letra != '\n')
+        {
+            fputc(letra, file);
+            scanf("%c", &letra);
+        }
+
+        rewind(file);
+
+        printf("\n\tTexto lido do arquivo:\n\n");
+        while(!feof(file))
+        {
+            letra = fgetc(file);
+            printf("%c", letra);
+        }
+        printf("\n");
+        fclose(file);
+    }
+    else
+    {
+        printf("\n\t-----> Erro ao abrir arquivo!\n");
+    }
+
+    return 0;
+}
